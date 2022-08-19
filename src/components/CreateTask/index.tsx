745c4b162e7112843f67038b81a812ca4fbb7098
Gabriel <biel_borel@hotmail.com>
2022-08-18 22:08:07 -0300

    CreateTask component created, which represents the Task Input and Create Task Button

---
+++ src/components/CreateTask/index.tsx
@@ -0,0 +1,33 @@
+import styles from './CreateTask.module.scss';
+import { PlusCircle } from 'phosphor-react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+export const CreateTaskArea = () => {
+  const [taskValue, setTaskValue] = useState('');
+
+  const handleTaskInputChange = (e: ChangeEvent<HTMLInputElement>) => setTaskValue(e.target.value);
+
+  const handleCreateNewTask = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setTaskValue('');
+    console.log(taskValue);
+  };
+
+  return (
+    <form onSubmit={handleCreateNewTask} className={styles.container}>
+      <input
+        value={taskValue}
+        onChange={handleTaskInputChange}
+        className={styles.inputTask}
+        type='text'
+        placeholder='Adicione uma nova tarefa'
+      />
+
+      <button className={styles.createTaskButton} type='submit'>
+        Criar
+        <PlusCircle size={20} />
+      </button>
+    </form>
+  );
+};
